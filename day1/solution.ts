96a0f46d23a9ffb8378d606ee4ec6e94735dd571
Jesper Berg <jesper.berg@cygni.se>
2023-12-01 13:33:19 +0100

    some day 1 refactoring during boring meeting

--- day1/solution.ts
+++ day1/solution.ts
@@ -2,17 +2,30 @@ import fs from 'fs';
 
 const input = fs.readFileSync('./input.txt').toString().split('\n');
 
+const numberMap = new Map([
+  ['one', '1'],
+  ['two', '2'],
+  ['three', '3'],
+  ['four', '4'],
+  ['five', '5'],
+  ['six', '6'],
+  ['seven', '7'],
+  ['eight', '8'],
+  ['nine', '9'],
+]);
+
+const getNumber = (numStr: string) => numberMap.get(numStr) || numStr;
+
 const getNumbersFromStrings = (
   numStrings: string[],
   doubleDigitRegex: RegExp,
-  singleDigitRegex: RegExp,
-  replaceFunc: (_: string) => string = (str) => str
+  singleDigitRegex: RegExp
 ) =>
   numStrings.map((val) => {
     const ddR = val.match(doubleDigitRegex);
-    if (ddR) return Number(`${replaceFunc(ddR[1])}${replaceFunc(ddR[2])}`);
-    const single = replaceFunc(val.match(singleDigitRegex)?.[0]!);
-    return Number(`${single}${single}`);
+    if (ddR) return Number(''.concat(...[1, 2].map((i) => getNumber(ddR[i]))));
+    const single = getNumber(val.match(singleDigitRegex)?.[0]!);
+    return Number(`${single.repeat(2)}`);
   });
 
 const solution1 = () => {
@@ -26,34 +39,19 @@ const solution1 = () => {
   return numbers.reduce((prev, curr) => prev + curr);
 };
 
-console.log(solution1());
-
 const solution2 = () => {
-  const strNumbers = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
-  const doubleDigitRegex =
-    /(\d|one|two|three|four|five|six|seven|eight|nine).*(\d|one|two|three|four|five|six|seven|eight|nine).*/;
-  const singleDigitRegex = /\d|one|two|three|four|five|six|seven|eight|nine/;
-
-  const repl = (str: string) =>
-    Object.keys(strNumbers).includes(str) ? strNumbers[str] : str;
+  const numRegex = '(\\d|one|two|three|four|five|six|seven|eight|nine)';
+  const doubleDigitRegex = new RegExp(`${numRegex}.*`.repeat(2));
+  const singleDigitRegex = new RegExp(numRegex);
 
   const numbers = getNumbersFromStrings(
     input,
     doubleDigitRegex,
-    singleDigitRegex,
-    repl
+    singleDigitRegex
   );
   return numbers.reduce((prev, curr) => prev + curr);
 };
+
+console.log(solution1());
 
 console.log(solution2());
